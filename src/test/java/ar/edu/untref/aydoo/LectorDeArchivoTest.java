aa341cd440074fd1623d569f53e81f4e430333e9
alejandroSchnidrig <ale.schnidrig@gmail.com>
2016-05-21 14:01:23 -0300

    se agrega test y clase lector de archivo

---
+++ src/test/java/ar/edu/untref/aydoo/LectorDeArchivoTest.java
@@ -0,0 +1,22 @@
+package ar.edu.untref.aydoo;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class LectorDeArchivoTest {
+
+	@Test
+	public void LeeyGuardaElArchivoEnUnaListaCorrectamente() throws IOException{
+		
+		LectorDeArchivo lector = new LectorDeArchivo();
+		lector.leerArchivo("/home/alejandro/aydoo-2016-e2/template/index.md");
+		
+		Assert.assertEquals("#esto es un titulo", lector.getContenido(0));
+		Assert.assertEquals("##esto es un sub-titulo", lector.getContenido(1));
+		Assert.assertEquals("i:estoEsUnaImagen.png", lector.getContenido(2));
+	}
+}
