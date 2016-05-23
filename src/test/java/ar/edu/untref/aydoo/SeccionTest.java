96d3255cd6dac238e302c75bc2074147efefd6b4
Emiliano <emilianoveppo@gmail.com>
2016-05-22 23:26:26 -0300

    Se agrega prueba a la clase SeccionTest

--- src/test/java/ar/edu/untref/aydoo/SeccionTest.java
+++ src/test/java/ar/edu/untref/aydoo/SeccionTest.java
@@ -10,28 +10,36 @@ public class SeccionTest {
 
 		Seccion unaSeccion = new Seccion("");
 		Elemento unTitulo = new Titulo("#El señor de los anillos");
-		
+
 		unaSeccion.agregarElemento(unTitulo);
 
 		Assert.assertEquals("<section>\n<h1>El señor de los anillos</h1>\n</section>", unaSeccion.imprimir());
 
 	}
-	
+
 	@Test
 	public void SeccionImprimeDosElementosCorrectamente() {
 
 		Seccion otraSeccion = new Seccion("");
 		Elemento unTitulo = new Titulo("#El Hobbit");
 		Elemento SubTitulo = new SubTitulo("##4000 años antes... ");
-		
-		
+
+
 		otraSeccion.agregarElemento(unTitulo);
 		otraSeccion.agregarElemento(SubTitulo);
-		
+
 
 		Assert.assertEquals("<section>\n<h1>El Hobbit</h1>\n<h2>4000 años antes... </h2>\n</section>", otraSeccion.imprimir());
 
 	}
-	
-	
+
+	@Test
+	public void SeccionSeImprimeSinElementosCorrectamente() {
+
+		Seccion otraSeccion = new Seccion("");
+
+		Assert.assertEquals("<section>\n</section>", otraSeccion.imprimir());
+
+	}
+
 }
