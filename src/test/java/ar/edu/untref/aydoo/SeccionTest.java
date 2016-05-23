7cacb3888fb3af90c217eb434acd9ccb25898794
alejandroSchnidrig <ale.schnidrig@gmail.com>
2016-05-22 23:44:56 -0300

    vuelvo a subir la clase SeccionTest con 3 test anteriormente commiteados ya que la elimine por error

---
+++ src/test/java/ar/edu/untref/aydoo/SeccionTest.java
@@ -0,0 +1,53 @@
+package ar.edu.untref.aydoo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SeccionTest {
+
+	@Test
+	public void SeccionImprimeUntituloCorrectamente() {
+
+		Seccion unaSeccion = new Seccion("");
+		Elemento unTitulo = new Titulo("#El señor de los anillos");
+
+		unaSeccion.agregarElemento(unTitulo);
+
+		Assert.assertEquals("<section>\n<h1>El señor de los anillos</h1>\n</section>", unaSeccion.imprimir());
+	}
+
+	@Test
+	public void SeccionImprimeDosElementosCorrectamente() {
+
+		Seccion otraSeccion = new Seccion("");
+		Elemento unTitulo = new Titulo("#El Hobbit");
+		Elemento SubTitulo = new SubTitulo("##4000 años antes... ");
+
+		otraSeccion.agregarElemento(unTitulo);
+		otraSeccion.agregarElemento(SubTitulo);
+
+		Assert.assertEquals("<section>\n<h1>El Hobbit</h1>\n<h2>4000 años antes... </h2>\n</section>",
+				otraSeccion.imprimir());
+
+	}
+
+	@Test
+	public void SeccionImprimeCuatroElementosCorrectamente() {
+
+		Seccion otraSeccion = new Seccion("");
+		Elemento unTitulo = new Titulo("#El Hobbit");
+		Elemento SubTitulo = new SubTitulo("##4000 años antes... ");
+		Elemento unaImagen = new Imagen("i:foto.png");
+		Elemento otroTitulo = new Titulo("#FIN");
+
+		otraSeccion.agregarElemento(unTitulo);
+		otraSeccion.agregarElemento(SubTitulo);
+		otraSeccion.agregarElemento(unaImagen);
+		otraSeccion.agregarElemento(otroTitulo);
+
+		Assert.assertEquals("<section>\n<h1>El Hobbit</h1>\n<h2>4000 años antes... </h2>\n<img src=\"foto.png\"/>\n"
+				+ "<h1>FIN</h1>\n</section>", otraSeccion.imprimir());
+
+	}
+
+}
