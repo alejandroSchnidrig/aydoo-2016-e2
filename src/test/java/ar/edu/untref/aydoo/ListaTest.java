64ec89f94145c335e308f5a02f2b45f799a46e91
Emiliano <emilianoveppo@gmail.com>
2016-05-25 15:11:04 -0300

    Se agrega test de lista enlazadas en ListaTest

--- src/test/java/ar/edu/untref/aydoo/ListaTest.java
+++ src/test/java/ar/edu/untref/aydoo/ListaTest.java
@@ -12,4 +12,20 @@ public class ListaTest {
 		
 		Assert.assertEquals("<lu>\n<li>algo</li>\n</lu>", lista.imprimir());
 	}
+	
+	@Test
+	public void ListaDentroDeOtraListaSeImprimeCorrectamente(){
+		
+		Lista lista = new Lista("*algo");
+		Lista segundaLista = new Lista("*mas");
+		Lista tercerLista = new Lista("*en");
+		Lista cuartaLista = new Lista("*lista");
+		
+		lista.agregarElemento(segundaLista);
+		lista.agregarElemento(tercerLista);
+		lista.agregarElemento(cuartaLista);
+		
+		Assert.assertEquals("<lu>\n<li>algo</li>\n<li>mas</li>\n<li>en</li>\n<li>lista</li>\n</lu>", lista.imprimir());
+	}
 }
+	
