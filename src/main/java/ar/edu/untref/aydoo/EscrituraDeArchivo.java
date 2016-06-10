a23c7da3849f8f2fe5d770cd693df8bb068a04cd
Emiliano <emilianoveppo@gmail.com>
2016-06-09 22:47:03 -0300

    Correccion de fix en la generacion del archivo

--- src/main/java/ar/edu/untref/aydoo/EscrituraDeArchivo.java
+++ src/main/java/ar/edu/untref/aydoo/EscrituraDeArchivo.java
@@ -3,19 +3,20 @@ package ar.edu.untref.aydoo;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
 
 public class EscrituraDeArchivo {
 
-	private BufferedWriter bw;
-	private PrintWriter wr;
+	public void grabarArchivoEnDirectorio(String nuevoArchivo, List<String> lineas) throws IOException{
+
+        FileWriter escribirArchivo = new FileWriter(nuevoArchivo);
+        BufferedWriter salidaArchivo = new BufferedWriter(escribirArchivo);
+        for(String PalabrasPorLinea: lineas){
+        	salidaArchivo.write(PalabrasPorLinea);
+        }
+        salidaArchivo.flush();
+        salidaArchivo.close();	
 	
-	public void grabarArchivoEnDirectorio(String nuevoArchivo,FileWriter escribirArchivo) throws IOException{
-		bw = new BufferedWriter(escribirArchivo);
-		wr = new PrintWriter(bw);   
-		wr.write(nuevoArchivo);
-		wr.close();
-		bw.close();
 	}
 
 }
--- src/main/java/ar/edu/untref/aydoo/GeneradorDeArchivo.java
+++ src/main/java/ar/edu/untref/aydoo/GeneradorDeArchivo.java
@@ -1,25 +1,37 @@
 package ar.edu.untref.aydoo;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GeneradorDeArchivo {
 
 	public void generarArchivoEnDirectorio(String rutaArchivo,String nuevaCadena) throws IOException{
-		EscrituraDeArchivo escribirArchivo = new EscrituraDeArchivo();
-		File directorioArchivo = new File(System.getProperty("user.dir"),rutaArchivo+"/index.html");
-		if(directorioArchivo.exists()){
-			directorioArchivo.delete();
-		}
-		directorioArchivo.createNewFile();
+		EscrituraDeArchivo modificarArchivo = new EscrituraDeArchivo();
 
-		FileWriter escritorDeArchivo = new FileWriter(directorioArchivo);
-		escribirArchivo.grabarArchivoEnDirectorio(nuevaCadena,escritorDeArchivo);
-			
-		
+		List<String> lineas = new ArrayList<String>();
+	    String unaLinea = null;
+	    String directorio = (System.getProperty("user.dir")+"/"+ rutaArchivo+"/index.html");
+	    System.out.println("El directorio es: " + directorio);
+		File directorioArchivo = new File(directorio);
+        FileReader fr = new FileReader(directorioArchivo);
+        BufferedReader br = new BufferedReader(fr);
+
+        while ((unaLinea = br.readLine()) != null) {
+            if (unaLinea.contains(("[este-es-el-texto-a-reemplazar]"))){
+            	unaLinea = unaLinea.replace("[este-es-el-texto-a-reemplazar]", nuevaCadena);
+            }
+            lineas.add(unaLinea+"\n");
+        }
+        fr.close();
+        br.close();
+
+        modificarArchivo.grabarArchivoEnDirectorio(directorio, lineas);
 	}
-	
+
 	public boolean existeArchivo(String ruta){
 		boolean existeEnLaRutaEspecificada=false;
 		File archivo = new File(ruta);
